c97554cfe405cda7810634b9dbcfc236b5282053
Shell-by <wnsghks1026@naver.com>
2023-09-17 15:56:04 +0900

    타입 정확하게 지정

--- findEmergency.php
+++ findEmergency.php
@@ -4,7 +4,7 @@ $serviceKey = "";
 $STAGE1 = $_GET['STAGE1'];
 $STAGE2 = $_GET['STAGE2'];
 $pageNo = $_GET['pageNo'];
-$type = $_GET['type'];
+$t = $_GET['type'];
 $numOfRows = 10;
 
 $ch = curl_init();
@@ -35,7 +35,7 @@ function xml_to_array($item) {
 
 for ($i = 0; $i < 10; $i++) {
     if (!isset($items->item[$i])) break;
-    if ($type === "e" && $data['emergenvyRoom'] <= 0) break;
+    if ($t == "e" && $items->item[$i]->hvec <= 0) continue;
     $data['emergenvyRoom'] = xml_to_array($items->item[$i]->hvec); //응급실
     $data['operatingRoom'] = xml_to_array($items->item[$i]->hvoc); //수술실
     $data['mri'] = xml_to_array($items->item[$i]->hvmriayn); //mri
@@ -43,6 +43,7 @@ for ($i = 0; $i < 10; $i++) {
     $data['ventilator'] = xml_to_array($items->item[$i]->hvventiayn); //인공호흡기
     $data['anbulance'] = xml_to_array($items->item[$i]->hvamyn); //구급차
     $data['dutyName'] = xml_to_array($items->item[$i]->dutyName); //병원 이름
+    $data['hpid'] = xml_to_array($items->item[$i]->hpid); //병원 id
     $data['dutyTel'] = xml_to_array($items->item[$i]->dutyTel3); //응급실 전화번호
     $json[$i] = $data;
 }
